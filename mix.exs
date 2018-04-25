d67534bf985d4c51ca92fe89db6a3d6b71f731d9
maneptha <maneptha@gmail.com>
2018-04-25 12:08:18 +0200

    Deployment to gcloud without distillery release.

--- mix.exs
+++ mix.exs
@@ -37,8 +37,7 @@ defmodule AppengineExample.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"},
-      {:distillery, "~> 1.5", runtime: false}
+      {:cowboy, "~> 1.0"}
     ]
   end
 end
